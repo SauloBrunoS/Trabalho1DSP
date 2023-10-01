5882805e68fe1d223b0ec346d166583d0ced7713
Luis <talkme.lusi@gmail.com>
2023-10-01 09:54:21 -0300

    teste

--- src/main/java/br/estudo/functions/CompactarCSVParaZIP.java
+++ src/main/java/br/estudo/functions/CompactarCSVParaZIP.java
@@ -7,18 +7,20 @@ public class CompactarCSVParaZIP {
     public static String compactarItemCSVParaZIP() throws Exception {
         String arquivoCSV = "entidade_item.csv";
         String arquivoZIP = "entidade_item.zip";
+
         FileOutputStream fos = new FileOutputStream(arquivoZIP);
         ZipOutputStream zipOut = new ZipOutputStream(fos);
-        FileInputStream fis = new FileInputStream(arquivoCSV);
+        FileInputStream input = new FileInputStream(arquivoCSV);
+        
         zipOut.putNextEntry(new ZipEntry(arquivoCSV));
         byte[] buffer = new byte[1024];
         int bytesRead;
-        while ((bytesRead = fis.read(buffer)) > 0) {
+        while ((bytesRead = input.read(buffer)) > 0) {
             zipOut.write(buffer, 0, bytesRead);
         }
         zipOut.closeEntry();
         zipOut.close();
-        fis.close();
+        input.close();
         return arquivoCSV + " foi compactado para " + arquivoZIP;
     }
 }
---
+++ src/main/java/br/estudo/functions/ConverterCSVtoJSONeXML.java
@@ -0,0 +1,4 @@
+package br.estudo.functions;
+public class ConverterCSVtoJSONeXML {
+    
+}
